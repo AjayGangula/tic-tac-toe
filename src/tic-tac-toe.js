84a2a3205bde717d10b60763f817caae55574d72
Ajaykumar Gangula <Ajaykumar_Gangula@epam.com>
2017-03-21 20:47:03 +0530

    Implemented the tic-tac-toe code challenge partially

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,35 @@
+var player;
+var i = 0;
+var oplayerField = [];
+var xplayerField = [];
+var restrictedValues = [];
+ 
 class TicTacToe {
     constructor() {
-
+        player = 'x';
+        oplayerField.splice(0, oplayerField.length);
+        i = 0;
     }
 
     getCurrentPlayerSymbol() {
-
+        return player;
     }
 
-    nextTurn(rowIndex, columnIndex) {
-
+    nextTurn(rowIndex, columnIndex) {      
+        if (oplayerField.length > 0) {
+            for (let j = 0; j <= oplayerField.length - 1; j++) {
+                var previousPlace = oplayerField[j];
+                var prevRowIndex = previousPlace.split('-')[0];
+                var prevColumnIndex = previousPlace.split('-')[1];
+                var prevSymbol = previousPlace.split('-')[2];
+                if ((prevRowIndex + '' + prevColumnIndex) === (rowIndex + '' + columnIndex)) {                    
+                    return;
+                }
+            }
+        }
+        player = this.getCurrentPlayerSymbol() === 'x' ? 'o' : 'x';
+        oplayerField[i]=rowIndex + '-' + columnIndex + '-' + player;
+        i++;        
     }
 
     isFinished() {
